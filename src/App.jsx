542b6249058e12ce06be899bb9444df21b299a01
tboy4all <taiyesuleiman09@gmail.com>
2024-05-02 16:59:39 +0100

    removed page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { Home } from "./component/Home";
-import Layout from "./component/Layout";
-import Message from "./component/Message";
-import MessageDetails from "./component/MessageDetails";
-import NotFound from "./component/NotFound";
-import TogglePage from "./component/TogglePage";
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Home } from './component/Home'
+import Layout from './component/Layout'
+import Message from './component/Message'
+import MessageDetails from './component/MessageDetails'
+import NotFound from './component/NotFound'
+import TogglePage from './component/TogglePage'
 
 function App() {
-
   return (
     <>
       <Routes>
@@ -16,11 +15,11 @@ function App() {
           <Route index element={<Home />} />
           <Route path='messages' element={<Message />} />
           <Route path='messages/:id' element={<MessageDetails />} />
-          <Route path='*' element={<Navigate to={<NotFound />} />} />
+          {/* <Route path='*' element={<Navigate to={<NotFound />} />} /> */}
         </Route>
       </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
